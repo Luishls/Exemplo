7d863eb867fd40611d964cce52776a2b15a46418
luis.hlsousa <luis.hlsousa@CAS511781W0732.senacsp.edu.br>
2017-02-21 20:35:22 -0300

    versão inicial

---
+++ teste_git/src/main/java/br/senac/tads/pi3/teste_git/Principal.java
@@ -0,0 +1,12 @@
+package br.senac.tads.pi3.teste_git;
+
+/**
+ *
+ * @author luis.hlsousa
+ */
+public class Principal {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
