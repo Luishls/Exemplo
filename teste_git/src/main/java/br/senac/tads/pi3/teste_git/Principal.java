b5f3cd2ec2220436249848b96651ab9be9869dde
luis.hlsousa <luis.hlsousa@CAS511781W0732.senacsp.edu.br>
2017-02-21 21:31:05 -0300

    mostrar quando i for par

--- teste_git/src/main/java/br/senac/tads/pi3/teste_git/Principal.java
+++ teste_git/src/main/java/br/senac/tads/pi3/teste_git/Principal.java
@@ -9,7 +9,9 @@ public class Principal {
     public static void main (String[] args) {
         
         for (int i=0; i < 10; i++){
+            if (i % 2 == 0){
         System.out.println("Hello World" + i);
+            }
         }
     }
 }
